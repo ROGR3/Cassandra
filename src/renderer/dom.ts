26b2e3d6117d291f376893b34a860a2407897ad6
borecjeborec1 <robingrundel@seznam.cz>
2022-04-02 16:38:21 +0200

    newlines

--- src/renderer/dom.ts
+++ src/renderer/dom.ts
@@ -14,9 +14,7 @@ document.addEventListener("keypress", (e): string => {
   if (e.key.toLowerCase() == "enter") {
     if (keyInp.value.length == 0 || valInp.value.length == 0) return helperText.innerText = "You need to fill in both fields!";
     if (shortcuts[keyInp.value]) return helperText.innerText = "This key already exists!";;
-    for (let i = 0; i < valInp.value.length; i++) {
-      if (valInp.value[i] == "\r") console.log("found newline")
-    }
+    valInp.value = decodeURIComponent(encodeURIComponent(valInp.value).replace(/%0A/g, "\\n"));
     shortcuts[keyInp.value.replace(/ /g, "\ ")] = valInp.value.replace(/ /g, "\ ");
     helperText.innerText = "Saved!";
     setTimeout(() => helperText.style.opacity = "0", 3000);
